7459377bec0edf94b9e578d59670a184dd44d2ea
Carlos Sanchez Perales <caer@ciencias.unam.mx>
2011-07-30 09:46:47 -0500

    Adding polymaps to project

---
+++ public/javascripts/maps.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	var po = org.polymaps;
+
+var map = po.map().container(document.getElementById("map").appendChild(po.svg("svg"))).add(po.interact());
+
+map.add(po.image().url(po.url("http://{S}tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/998/256/{Z}/{X}/{Y}.png").hosts(["a.", "b.", "c.", ""])));
+});
